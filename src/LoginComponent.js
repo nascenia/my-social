fe6b85a1a20ef819db14adb281fb310da0046dff
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2017-07-12 11:40:43 +0600

    create service module for accessing api

--- src/LoginComponent.js
+++ src/LoginComponent.js
@@ -1,7 +1,7 @@
 import React, { Component} from 'react';
 import FacebookProvider, { Login } from 'react-facebook';
 import { Button } from 'react-bootstrap';
-var axios = require('axios');
+var Service = require('./utils/service');
 
 export default class LoginComponent extends Component {
   constructor(props) {
@@ -12,33 +12,19 @@ export default class LoginComponent extends Component {
   handleResponse = (data) => {
     this.props.profile(data.profile);
     console.log(data);
-    //this.getEvents(data.profile.id, data.tokenDetail.accessToken);
+    console.log('-------- facebook feed');
+    console.log(Service.facebookFeed(data.profile.id, data.tokenDetail.accessToken));
   }
 
   handleError = (error) => {
     this.setState({ error });
   }
 
-  getEvents = (u_id, access_token) =>{
-    console.log('--with is getEvents with u_id' + u_id);
-    axios.get('https://graph.facebook.com/v2.9/'+ u_id + '/friends?access_token=' + access_token)
-      .then(response => {
-           console.log(" axios call success ");
-           console.log(response);
-           this.setState({
-             friends: response.data.data
-           });
-      })
-      .catch(function (error) {
-        console.log("axios call fail" + error);
-      });
-  }
-
   render() {
     return (
       <FacebookProvider appId="152690645277827">
         <Login
-          scope="email,user_friends,user_events"
+          scope="email,user_friends,user_events,user_posts"
           onResponse={this.handleResponse}
           onError={this.handleError}
         >
